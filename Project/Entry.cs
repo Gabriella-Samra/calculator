0b9ba490e86497556d7ef79a74b4c11c6e782f2f
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2024-05-16 17:10:53 +0100

    string interpolation on equation and results print, refactored prompt for numbers and characters

--- Project/Entry.cs
+++ Project/Entry.cs
@@ -13,35 +13,14 @@ namespace Calculator.Project
         {
             double? entry1 = null;
             double? entry2 = null;
-            string? answeredOperator;
             string formulaOperator;
 
-            // var numberOne = PromptForDouble("Enter your first number: ");
-            // var numberTwo = PromptForDouble("Enter your second number: ");
-            // var calculationOperator = PromptForCharacter("What type of equation would you like to do? (enter one of the following: " + String.Join(Calc.Operators));
-
-            Console.WriteLine("Enter your first number: ");
-            entry1 = TypeModifier.StringToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter your second number: ");
-            entry2 = TypeModifier.StringToDouble(Console.ReadLine());  
-
-            Console.WriteLine("What type of equation would you like to do? (enter one of the following: " + Calc.opertorListWithCommas);
+            entry1 = Prompt.PromptForDouble("first");
+            entry2 = Prompt.PromptForDouble("second");
+            formulaOperator = Prompt.PromptForOperator();
             
-            // do the same as the questions above with the readline
-            answeredOperator = Console.ReadLine();
-            if (Calc.CheckOperatorMatchesAllowedOperators(answeredOperator))
-            {
-                formulaOperator = answeredOperator;
-            }    
-            else
-            {
-                throw new ArgumentException("The requested operator cannot be handled within this calculator, please type only the following types: " + Calc.opertorListWithCommas);
-            }    
-
-            // use string interpolation
-            Console.WriteLine("The equation you want me to do is: " + entry1 + " " + formulaOperator + " " + entry2);
-            Console.WriteLine("The result is: " + Calc.EquationCalculatorDecider(entry1.Value, entry2.Value, formulaOperator));
+            Console.WriteLine($"The equation you want me to do is: {entry1} {formulaOperator} {entry2}");
+            Console.WriteLine($"The result is: {Calc.EquationCalculatorDecider(entry1.Value, entry2.Value, formulaOperator)}");
         }
     }
 }
---
+++ Project/Prompt.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculator.Project
+{
+    public class Prompt
+    {
+        private static string? answeredOperator;
+
+        public static double PromptForDouble(string value)
+        {
+            Console.WriteLine("Enter your " + value + " number: ");
+            double response = TypeModifier.StringToDouble(Console.ReadLine());
+            return response;
+        }
+
+        public static string PromptForOperator()
+        {
+            Console.WriteLine("What type of equation would you like to do? (enter one of the following: " + Calc.opertorListWithCommas);
+            
+            answeredOperator = Console.ReadLine();
+            if (Calc.CheckOperatorMatchesAllowedOperators(answeredOperator))
+            {
+                return answeredOperator;
+            }    
+            else
+            {
+                throw new ArgumentException($"The requested operator: \"{answeredOperator}\" cannot be handled within this calculator, please enter only one of the following types: {Calc.opertorListWithCommas}");
+            }    
+        }
+    }
+}
