2af479fb44d8ab8322ced92411fbc8cf44c3ca6a
Gabriella-Samra <gabriella.samra@hotmail.co.uk>
2024-05-16 17:17:59 +0100

    cleaned up when declaring variables

--- Project/Entry.cs
+++ Project/Entry.cs
@@ -11,13 +11,9 @@ namespace Calculator.Project
     {
         public static void Main(string[] arguments)
         {
-            double? entry1 = null;
-            double? entry2 = null;
-            string formulaOperator;
-
-            entry1 = Prompt.PromptForDouble("first");
-            entry2 = Prompt.PromptForDouble("second");
-            formulaOperator = Prompt.PromptForOperator();
+            double? entry1 = Prompt.PromptForDouble("first");
+            double? entry2 = Prompt.PromptForDouble("second");
+            string formulaOperator = Prompt.PromptForOperator();
             
             Console.WriteLine($"The equation you want me to do is: {entry1} {formulaOperator} {entry2}");
             Console.WriteLine($"The result is: {Calc.EquationCalculatorDecider(entry1.Value, entry2.Value, formulaOperator)}");
